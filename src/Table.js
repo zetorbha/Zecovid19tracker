b1065cd4dea72663dff9887315f653bed3431e66
zetorbha <zebhadra@gmail.com>
2020-07-29 08:49:40 +0530

    flag update

--- src/Table.js
+++ src/Table.js
@@ -5,8 +5,9 @@ import numeral from "numeral";
 const Table = ({countries}) => {
     return (
         <div class="table">
-            {countries.map(({country, cases})=>(
-               <tr>
+            {countries.map(({country, cases, countryInfo})=>(
+               <tr> 
+                <td> <img src={countryInfo.flag} alt={countryInfo.iso2}/></td>
                 <td>{country}</td>
                 <td><strong>{numeral(cases).format("0,0")}</strong></td>
                 </tr>
